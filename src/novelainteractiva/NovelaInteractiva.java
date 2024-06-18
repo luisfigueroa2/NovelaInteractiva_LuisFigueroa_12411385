873a69586a9024e6c44651678fc8690ca5091750
luisfigueroa2 <luisfigueroahn2276@gmail.com>
2024-06-17 21:00:23 -0600

    cuarto commit

--- src/novelainteractiva/NovelaInteractiva.java
+++ src/novelainteractiva/NovelaInteractiva.java
@@ -3,187 +3,273 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package novelainteractiva;
-
 /**
  *
  * @author luisf
  */
 /*GLOSARIO DE VARIABLES:
--entrada: Scanner
--paciencia: variable int que define la pacienca (siempre empieza como 0)
--maxpac: variable int que define la paciencia maxima (si sobrepase durante el juego se acaba)
--escenas: variable ArrayList que guarda las escenas del juego (avanza con un contador)
--acto: variable int que guarda que escena se esta jugando
--rand: variable random para los parametros de la paciencia
--continuar: variable boolean que lee cuando se acaba el juego por medio de paciencia
--nombre: el nombre del usuario
--opcion: variable tipo int usada para visualizar las opciones que tiene el usuario en el JOptionPane
--fin: boolean que controla cuando el juego se acaba por medio de llegar al maximo de escenas
 
 GLOSARIO DE METODOS:
--generarmaxpac: usa el random y dos parametros para generar la paciencia maxima de la partida
--inicio: este metodo crea las escenas agregandolas al arraylist
--iniciarvideojogo: metodo que inicia el juego pidiendole al usuario su nombre y terminando el juego al llegar al fin
--mostrarops: usa JOptionPane para mostrarle las opciones de dialogo al usuario y regresar su respuesta
--choose!: este metodo es el que tiene todos los switches que controlan como avanza el juego segun el dialogo
--gameu: este metodo recursivo es el que controla la paciencia del usuario, repitiendose hasta que se acaba la paciencia, el usuario decide no continuar o responde mal
 
--EL USO DE GUI ES BASTANTE BASICO EN ESTE AVANCE, CONTINUARE ESTUDIANDO COMO HACER UNA INTERFAZ COMPLETA PARA LA ENTREGA FINAL
 
-HERRAMIENTAS USADAS EN ESTE AVANCE:
-Estructuras de control: secuenciales, de decision, de repeticion.
-Tres clases
-Metodos que retornen y recibanenteros, reales y booleanos.
-Metodos que reciban cadenas
-Metodos estaticos y no estaticos
-RANDOM
-Constructor
-Arreglos unidimensionales
-Arraylist
-GUI (INCOMPLETO)
-TRES FORMAS DE IMPRIMIR (Use ln, sencilla y f%)
-JOptionPane
-Scanner
-Diferentes tipos de variables (locales y de referencia)
-Getters y Setters
 */
+import java.awt.*; //IMPORT PARA GUI
+import java.awt.event.*;//IMPORT PARA USAR ACTION LISTENER
+import javax.swing.*; //IMPORT PARA EL GUI (FRAME, PANELES, LABELS, BOTONES, SWING.UTILITIES)
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
-import java.util.ArrayList;
 
-class NovelaInteractiva {
+class NovelaInteractiva {//INICIALIZA LOS VARIABLES IMPORTANTES
     static Scanner entrada = new Scanner(System.in);
     private int paciencia;
     private int maxpac;
     private ArrayList<Escena> escenas;
     private int acto;
     private boolean fin;
+    private Jugador jugador;
+    private JFrame gui;
+    private JLabel dialogoLabel;
+    private JButton[] opcionBoton;
+    private JLabel pacienciaLabel;
     
-    public static void main(String[] args) {
-        jogo hola = new jogo();
-        hola.setTitle("sexo");
-        hola.setVisible(true);
-        NovelaInteractiva jogo = new NovelaInteractiva();
-        jogo.iniciarvideojogo();
-    }
-    // Constructor que inicializa la paciencia en 0 y asigna la paciencia maxima aleatoria
-    public NovelaInteractiva() {
-        paciencia = 0;
-        maxpac = generarmaxpac(1, 3);
+    
+    public static void main(String[] args) {//METODO MAIN
+        String nombre = JOptionPane.showInputDialog("Por favor, introduce tu nombre:");
+        Jugador jugador = new Jugador(nombre);
+        SwingUtilities.invokeLater(() -> new NovelaInteractiva().iniciarvideojogo(jugador));
+    }//FIN MAIN
+
+    public NovelaInteractiva() {//INICIO CREACION GUI
         escenas = new ArrayList<>();
-        acto = 0;
-        fin = false;
-        inicio();
+        //CREACION DE LA WINDOW FRAME
+        gui = new JFrame("Novela Interactiva");
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//POR SI EL USUARIO CIERRA LA VENTANA
+        gui.setSize(400, 300);
+        
+        //CREACION DEL LABEL DE DIALOGO
+        dialogoLabel = new JLabel(" ");
+        gui.add(dialogoLabel, BorderLayout.NORTH);
+        
+        //CREACION DEL PANEL DE OPCIONES
+        JPanel opcionesPanel = new JPanel();
+        
+        //CREACION DE LOS BOTONES DE OPCIONES
+        opcionesPanel.setLayout(new GridLayout(3, 1));
+        opcionBoton = new JButton[3];
+        for (int i = 0; i < 3; i++) {
+            opcionBoton[i] = new JButton();
+            opcionBoton[i].addActionListener(new boton());
+            opcionesPanel.add(opcionBoton[i]);
+        }
+        gui.add(opcionesPanel, BorderLayout.CENTER);
+        //FIN CREACION DE OPCIONES
+        
+        //CREACION DEL LABEL DE PACIENCIA
+        pacienciaLabel = new JLabel("Paciencia: 0");
+        gui.add(pacienciaLabel, BorderLayout.SOUTH);
+        gui.setVisible(true);
+        inicio();//YA SE INICIA EL JUEGO CON TODO SU GUI
     }
 
-    private int generarmaxpac(int min, int max) {
+    public void iniciarvideojogo(Jugador jugador) {//METODO PARA INICIAR EL JUEGO
+        this.jugador = jugador;
+        reiniciarJuego();
+        actualizarEscena();
+    }//FIN METODO DE INICIAR EL JUEGO
+
+    public static int generarMaxPac(int min, int max) {//METODO PARA GENERAL LA PACIENCIA
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
-    }
+    }//FIN METODO GENERAR PACIENCIA MAXIMA
 
-    private void inicio() {
+    public void inicio() {//METODO QUE AGREGA LAS ESCENAS AL ARRAY LIST DE ESCENAS
         escenas.add(new Escena("Ayudame a comprar leche", new String[]{"No quiero", "Vaya pue", "..."}));
         escenas.add(new Escena("Ya estan adentro de la tienda, por alguna razon el niño se quedo paralizado frente al cajero...No se mueve y hay gente en la fila esperando.", new String[]{"Tranquilo, dale el dinero", "Tan inutil eres?", "La gente espera, apurate"}));
         escenas.add(new Escena("*Nos cuenta un poco de su historia*.", new String[]{"Sos un rarito", "*escuchar*", "No quiero saberlo"}));
-        //la ultima escena esta si va a llevar toda su backstory
-        //la historia ya esta completa, solo ocupo ver todos los temas para seguir implementandola para la entrega final
-    }
+        escenas.add(new Escena("Ayudame a comprar leche", new String[]{"No quiero", "Vaya pue", "..."}));
+        escenas.add(new Escena("Ya estan adentro de la tienda, por alguna razon el niño se quedo paralizado frente al cajero...No se mueve y hay gente en la fila esperando.", new String[]{"Tranquilo, dale el dinero", "Tan inutil eres?", "La gente espera, apurate"}));
+        escenas.add(new Escena("*Nos cuenta un poco de su historia*.", new String[]{"Sos un rarito", "*escuchar*", "No quiero saberlo"}));
+    }//FIN METODO INICIO
 
-    public void iniciarvideojogo() {
-        String nombre = JOptionPane.showInputDialog("Por favor, introduce tu nombre:");
-        Jugador jugador = new Jugador(nombre);
-        gameu();
-
-        if (fin) {
-            System.out.println("Si mama...");
-        } else {
-            System.out.println("Parece que " + nombre + " no me sirve... probemos algo mas...");
-        }
-    }
-    private void gameu(){
-            if (paciencia > maxpac) {
-                System.out.println("Has perdido demasiada paciencia....");
-                return;
-            }else if (acto >= escenas.size() && paciencia <= maxpac) {
-                System.out.println("Compraste la leche..?");
-                fin = true;
-                return;       
-            }
-            String opcion = mostrarops();
-            choose(opcion);
+    public void actualizarEscena() {//INICIO METODO PARA MOVERSE ATRAVES DEL JUEGO
+        
+        if (paciencia > maxpac) {//CONDICION DE SALIDA #1: PACIENCIA GAME OVER 
+            JOptionPane.showMessageDialog(gui, "Has perdido demasiada paciencia....");
+            JOptionPane.showMessageDialog(gui, "Parece que no me sirves...probemos algo mas...");
+            finJuego();
+            return;
+        } else if (acto >= escenas.size() && paciencia <= maxpac) {//CONDICION DE SALIDA #2: FIN DEL JUEGO
+            JOptionPane.showMessageDialog(gui, "Compraste la leche..?");
             
-            if (paciencia > maxpac){
-                System.out.println("Has perdido demasiada paciencia....");
-                return;
-            }
-             System.out.printf("Cuidado con la paciencia, no dejes que suba... Paciencia actual: %d/%d%n", paciencia, maxpac);
-             boolean continuar = false;
-                while (!continuar) {
-                    System.out.print("Deseas continuar? (s/n): ");
-                    char resp = entrada.next().charAt(0);
-                    if (resp == 'n' || resp == 'N') {
-                        return;
-                    } else if (resp == 's' || resp == 'S') {
-                        continuar = true;
-                    } else {
-                        System.out.println("Oye, tomate esto en serio, ingresa lo que te dicen");
+                    //MATRIZ DE CARITA FELIZ YIPPEEEE
+                    int row = 7;
+                    int col = 7;
+                    char[][] smiley = new char[row][col];
+                     for (int i = 0; i < row; i++) {
+                     for (int j = 0; j < col; j++) {
+                        smiley[i][j] = ' ';
                     }
-            }//fin while continuar
-            gameu();
-        }//fin recursiva
-        
-        
-    
-    public String mostrarops() {
-        Escena escena = escenas.get(acto);
-        String[] opciones = escena.getOpciones();
-        int opcion = JOptionPane.showOptionDialog(null, escena.getDialogo(),"Novela Interactiva", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-        return opciones[opcion];
-    }
+                }
 
-    public void choose(String opcion) {
+                    smiley[2][2] = 'O';
+                    smiley[2][4] = 'O';
+                    smiley[3][1] = '-';
+                    smiley[4][2] = '-';
+                    smiley[4][3] = '-';
+                    smiley[4][4] = '-';
+                    smiley[3][5] = '-';
+
+                for (int i = 1; i < col - 1; i++) {
+                    smiley[0][i] = '*';
+                    smiley[row - 1][i] = '*';
+                }
+                for (int i = 1; i < row - 1; i++) {
+                    smiley[i][0] = '*';
+                    smiley[i][col - 1] = '*';
+                }
+
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        System.out.print(smiley[i][j] + " ");
+                    }
+                System.out.println();
+                }
+                
+            System.out.println("Gracias por jugar " + jugador.toString());
+            System.out.println("---------------------------");
+            System.out.printf("Terminaste el juego con una cantidad de paciencia de %d/%d%n", paciencia, maxpac);
+            if (paciencia == maxpac) {
+                System.out.println("A nada va? se mas amable a la proxima...");
+            }
+            System.out.println("---------------------------");
+            System.out.println("Te gusto el juego?");
+            String resp = entrada.next();
+            if (resp.equalsIgnoreCase("no")) {
+                //MATRIZ CARITA TRISTE :(
+                for (int i = 0; i < row; i++) {
+                     for (int j = 0; j < col; j++) {
+                        smiley[i][j] = ' ';
+                    }
+                }
+
+                    smiley[2][2] = 'O';
+                    smiley[2][4] = 'O';
+                    smiley[5][1] = '-';
+                    smiley[4][2] = '-';
+                    smiley[4][3] = '-';
+                    smiley[4][4] = '-';
+                    smiley[5][5] = '-';
+
+                for (int i = 1; i < col - 1; i++) {
+                    smiley[0][i] = '*';
+                    smiley[row - 1][i] = '*';
+                }
+                for (int i = 1; i < row - 1; i++) {
+                    smiley[i][0] = '*';
+                    smiley[i][col - 1] = '*';
+                }
+
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        System.out.print(smiley[i][j] + " ");
+                    }
+                System.out.println();
+                }
+            }
+            finJuego();
+            return;
+        }//FIN DEL JUEGO
+
+        Escena escena = escenas.get(acto);
+        dialogoLabel.setText(escena.getDialogo());
+
+        String[] opciones = escena.getOpciones();
+        for (int i = 0; i < opcionBoton.length; i++) {
+            if (i < opciones.length) {
+                opcionBoton[i].setText(opciones[i]);
+                opcionBoton[i].setVisible(true);
+            } else {
+                opcionBoton[i].setVisible(false);
+            }
+        }
+
+        pacienciaLabel.setText("Paciencia: " + paciencia + "/" + maxpac);
+    }
+    
+    public void reiniciarJuego() {//METODO PARA INICIALIZAR LOS VARIABLES USADOS DURANTE EL JUEGO Y REINICIARLOS AL TERMINAR
+        paciencia = 0;
+        maxpac = generarMaxPac(1, 3);
+        acto = 0;
+        fin = false;
+    }//FIN METODO RESTART
+    
+    public void finJuego() {//METODO PARA PEDIRLE AL USUARIO SI QUIERE JUGAR OTRA VEZ
+        fin = true;
+        gui.dispose();//AL LLEGAR AQUI EL GUI SE CIERRA
+        System.out.println("Deseas jugar otra vez? si/no");
+        String resp2 = entrada.next();
+        if (resp2.equalsIgnoreCase("si")) {
+            SwingUtilities.invokeLater(() -> new NovelaInteractiva().iniciarvideojogo(jugador));
+        } else {
+            System.exit(0);
+        }
+    }//FIN METODO JUEGO
+    
+    //ESTA CLASE ES LA QUE HACE QUE LOS BOTONES HAGAN SUS ACCIONES
+    private class boton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton source = (JButton) e.getSource();
+            choose(source.getText());
+            actualizarEscena();
+        }
+    }//FIN CLASE BOTON
+    
+  
+    public void choose(String opcion) { //METODO DE TODAS LAS OPCIONES
         Escena escena = escenas.get(acto);
         switch (opcion) {
             case "No quiero":
-                System.out.println("Se supone que me ayudes.");
+                JOptionPane.showMessageDialog(gui, "Se supone que me ayudes.");
                 paciencia++;
                 break;
             case "Vaya pue":
-                System.out.println("Bueno pues vamos.");
+                JOptionPane.showMessageDialog(gui, "Bueno pues vamos.");
                 break;
             case "...":
-                System.out.println("Por favor dime algo, no te quedes callado... ayudame...");
+                JOptionPane.showMessageDialog(gui, "Por favor dime algo, no te quedes callado... ayudame...");
                 paciencia++;
                 break;
             case "Tranquilo, dale el dinero":
-                System.out.println("Ah, gracias, que haria sin ti.");
+                JOptionPane.showMessageDialog(gui, "Ah, gracias, que haria sin ti.");
                 break;
             case "Tan inutil eres?":
-                System.out.println("El niño escucho tu insulto y pago, funciono pero no creo que este feliz.");
+                JOptionPane.showMessageDialog(gui, "El niño escucho tu insulto y pago, funciono pero no creo que este feliz.");
                 paciencia++;
                 break;
             case "La gente espera, apurate":
-                System.out.println("Ah, si, ya voy.");
+                JOptionPane.showMessageDialog(gui, "Ah, si, ya voy.");
                 break;
             case "Sos un rarito":
-                System.out.println("...");
+                JOptionPane.showMessageDialog(gui, "...");
                 paciencia = 99;
                 break;
             case "*escuchar*":
-                System.out.println("El niño sigue contando su historia.");
+                JOptionPane.showMessageDialog(gui, "El niño sigue contando su historia.");
                 break;
             case "No quiero saberlo":
-                System.out.println("Perdon...");
+                JOptionPane.showMessageDialog(gui, "Perdon...");
                 paciencia++;
                 break;
             default:
-                System.out.println("Opción no válida.");
+                JOptionPane.showMessageDialog(gui, "Opción no válida.");
                 break;
         }
         acto++;
-    }
+    }//FIN METODO OPCIONES
 }
+
 
 
 
